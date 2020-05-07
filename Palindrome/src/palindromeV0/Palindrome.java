1dbcb5b64eaa1eccef0402726df14df1e32b4a17
Jim Lee <bass-guy@cfl.rr.com>
2020-05-06 23:31:46 -0400

    Add comments

--- Palindrome/src/palindromeV0/Palindrome.java
+++ Palindrome/src/palindromeV0/Palindrome.java
@@ -7,15 +7,19 @@ public class Palindrome {
 		StringBuilder sbForward;
 		StringBuilder sbReverse;
 		boolean isPalindrome = true;
-				
+
 		for (String item : words) {
-			// Remove whitespaces characters, if any, from input since we are testing for palindrome words
+
+			// Remove whitespaces characters, if any, from input since we are testing for
+			// palindrome words
 			strIn = item.replaceAll("\\s+", "").toLowerCase();
+			
 			// Convert word from String to StringBuilder
 			sbForward = new StringBuilder(strIn);
+			
 			// Create a reverse version of StringBuilder version
 			sbReverse = sbForward.reverse();
-			
+
 			// Test the compare the word to it's reverse version
 			if (!strIn.equals(sbReverse.toString())) {
 				isPalindrome = false;
