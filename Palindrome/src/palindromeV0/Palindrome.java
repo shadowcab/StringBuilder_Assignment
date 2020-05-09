d8b9a7ce9a10f5acddb03a7f300f9bad46c80686
Jim Lee <bass-guy@cfl.rr.com>
2020-05-09 02:59:46 -0400

    Improve comments and display failed palindrome candidate followed by candidate in reversed order.  This is version submitted for homework.

--- Palindrome/src/palindromeV0/Palindrome.java
+++ Palindrome/src/palindromeV0/Palindrome.java
@@ -10,21 +10,23 @@ public class Palindrome {
 
 		for (String word : words) {
 
-			// Remove all punctuation and whitespace in word item before processing
-			// This is based on Dictionary.com's definition for palindrome
+			// Remove all punctuation and whitespace in "word" and convert to lower case before 
+			// testing for palindrome.  This constraint is based on Dictionary.com's definition 
+			// and examples of palindromes (https://www.dictionary.com/browse/palindrome).
 			strWord = word.replaceAll("\\p{Punct}|\\s+", "").toLowerCase();
 			
-			// Convert String item to StringBuilder type
+			// Convert (String) word to StringBuilder type
 			sbWord = new StringBuilder(strWord);
 			
-			// Reverse character order in sbItem
+			// Reverse character order in (StringBuilder) word
 			sbWordReversed = sbWord.reverse();
 
 			// Compare word to reversed word.  Note: all words are considered palindromes until proven
 			// otherwise. If not a palindrome, set flag to false.
 			if (!strWord.equals(sbWordReversed.toString())) {
 				isPalindrome = false;
-				System.out.println("Failed on: " + sbWordReversed.toString() + " " + strWord);
+				// Display the failed palindrome candidates in normal and reversed order
+				System.out.println("Failed on: " + strWord + " " + sbWordReversed.toString());
 			}
 		}
 		System.out.println();
--- Palindrome/src/palindromeV0/TestPalindrome.java
+++ Palindrome/src/palindromeV0/TestPalindrome.java
@@ -1,9 +1,9 @@
 package palindromeV0;
 
 public class TestPalindrome {
-	public static void main(String[] args) {
-		Palindrome palindrome = new Palindrome();
-		boolean result = palindrome.palindromChecker("", "a", "aa", "bb", "aba", "bab", "bob", "1- b -1", "Madam, I'm Adam", "Poor Dan is in a droop", "dud");
-		System.out.println("All strings entered were palindromes: " + result);
-	}
+    public static void main(String[] args) {
+        Palindrome palindrome = new Palindrome();
+        boolean result = palindrome.palindromChecker("bob", "Sue", "aaa", "Gossai");
+        System.out.println("All strings entered were palindromes: " + result);
+    }
 }
